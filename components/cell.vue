435fc158c7c85d5fd19989e12d16c72ef484a01f
Alex Gustafsson <alex-gustafsson@live.se>
2018-04-02 18:28:19 +0200

    Made hint cells square

--- components/cell.vue
+++ components/cell.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="cell" v-bind:class="{selected: selected, highlit: highlit, secondary: secondarySelected}" v-on:click="clicked">
     <div class="grid hints" v-bind:class="{hide: number && !secondarySelected}">
-      <p v-for="i in 9" v-bind:class="{hide: !marks[i], highlit: highlitMarks[i]}">{{i}}</p>
+      <div v-for="i in 9" v-bind:class="{hide: !marks[i], highlit: highlitMarks[i]}">
+        <p>{{i}}</p>
+      </div>
     </div>
     <p v-bind:class="{translucent: secondarySelected}">{{number}}</p>
   </div>
